137e4d951dd180d3e719d562e06be8e59f0f53a9
Ayush Verma <ayush02av@gmail.com>
2024-09-05 09:59:23 +0530

    Ping request

--- app/webhook/routes.py
+++ app/webhook/routes.py
@@ -3,6 +3,12 @@ from ..extensions import mongo
 
 webhook = Blueprint('Webhook', __name__, url_prefix='/webhook')
 
+@webhook.route('/ping', methods=["GET"])
+def ping():
+    return {
+        "message": "Hello World"
+    }, 200
+
 @webhook.route('/receiver', methods=["POST"])
 def receiver():
     request_json = request.get_json()
